724ea450c0fc7cea436768dc0806d126aac86c06
Steven <stevenjacobprice@gmail.com>
2018-04-09 00:02:53 -0500

    add client js console logging

--- public/client.js
+++ public/client.js
@@ -2,4 +2,18 @@
   console.log('client changes');
   // define variables that reference elements on our page
   const displayName = document.getElementById('display_name');
+  const getName = document.getElementById('get_name');
+  console.log({
+    displayName: displayName,
+    getName: getName
+  });
+  displayName.innerHTML = '<p>Ax Wiki</p>';
+  getName.addEventListener('click', function (event) {
+    console.log({
+      event: event,
+      getName: true
+    })
+    let currentTime = new Date();
+    displayName.innerHTML = '<p>Ax Wiki <br>' + currentTime + '</p>';
+  });
 })();
