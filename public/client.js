9d37afc520e01dfaf7a17b09a50cb28445d76e75
Steven <stevenjacobprice@gmail.com>
2018-04-09 00:11:40 -0500

    add axios to index

--- public/client.js
+++ public/client.js
@@ -9,11 +9,20 @@
   });
   displayName.innerHTML = '<p>Ax Wiki</p>';
   getName.addEventListener('click', function (event) {
+    let names = axios.get('/name')
+    .then(function (res) {
+      return res.data;
+    })
+      // TODO: parse data
+    .catch(function (error) {
+      console.log(error);
+    });
+    let currentTime = new Date();
     console.log({
       event: event,
-      getName: true
-    })
-    let currentTime = new Date();
+      getName: true,
+      names: names
+    });
     displayName.innerHTML = '<p>Ax Wiki <br>' + currentTime + '</p>';
   });
 })();
