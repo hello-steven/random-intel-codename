c3346f18ddcda872355dc7a565919eb1bd6a05f9
Travis Shears <btbtravis@gmail.com>
2018-04-14 18:41:08 -0500

    Add shuffle feature to front and backend

--- test/names.test.js
+++ test/names.test.js
@@ -7,11 +7,13 @@ import createNames from '../lib/names';
 import jsdom from 'jsdom';
 import axios from 'axios';
 import R from 'ramda';
+import Foswig from 'foswig';
 
 const realServices = {
   axios: axios,
   jsdom: jsdom.JSDOM,
-  R:R
+  R:R,
+  Foswig: Foswig
 };
 describe('Names Module', function () {
   //let realNamesAPI = createNames(realServices)();
@@ -19,30 +21,25 @@ describe('Names Module', function () {
   beforeEach(function() {
     realNamesAPI = createNames(realServices)();
   });
-  it('kebabCase swaps space for dash', () => {
-    return realNamesAPI.then(function (api) {
-      let name = api.toKebabCase('TEST test');
-      expect(name).to.be.an('string');
-      expect(/-/g.test(name)).to.equal(true);
-    });
-  });
-  it('kebabCase trims start and end spaces', () => {
-    return realNamesAPI.then(function (api) {
-      let name = api.toKebabCase('  TEST test  ');
-      expect(name).to.be.an('string');
-      expect(name[0] !== '-').to.equal(true);
-      expect(name[name.length] !== '-').to.equal(true);
-    });
-  });
   it('createGetName returns a promise', () => {
     expect(realNamesAPI).to.be.an('promise');
   });
-  it('getName returnes a names', () => {
+  it('getName returnes a name', () => {
     return realNamesAPI.then(function (api) {
       let names = api.getName();
-      expect(names).to.be.an('object');
-      expect(names).to.have.property('name');
-      expect(names).to.have.property('kebabCase');
+      expect(names).to.be.an('string');
+    });
+  });
+  it('getKebabCase returnes a name', () => {
+    return realNamesAPI.then(function (api) {
+      let names = api.getKebabCaseName();
+      expect(names).to.be.an('string');
+    });
+  });
+  it('getShuffledName returnes a name', () => {
+    return realNamesAPI.then(function (api) {
+      let names = api.getShuffledName();
+      expect(names).to.be.an('string');
     });
   });
 });
--- test/server.test.js
+++ test/server.test.js
@@ -19,13 +19,13 @@ describe('Server Module', function () {
         .then(test => {
           server.close();
           return test;
-        });
+        }).catch(e => server.close());
     });
   });
   it('/name returns a name', () => {
     return serverPromise.then(server => {
       return supertest(server)
-        .get('/name')
+        .get('/kebab')
         .expect(200)
         .then(res => {
           let name = res.text;
@@ -36,10 +36,9 @@ describe('Server Module', function () {
         .then(test => {
           server.close();
           return test;
-        });
+        }).catch(e => server.close())
     });
   });
-
   it('/kebab return a kebab', () => {
     return serverPromise.then(server => {
       return supertest(server)
@@ -54,7 +53,22 @@ describe('Server Module', function () {
         .then(test => {
           server.close();
           return test;
-        });
+        }).catch(e => server.close());
+    });
+  });
+  it('/shuffle return a kebab', () => {
+    return serverPromise.then(server => {
+      return supertest(server)
+        .get('/shuffle')
+        .expect(200)
+        .then(res => {
+          server.close();
+          let name = res.text;
+          expect(name).to.be.a('string');
+          expect(/{/g.test(name)).to.equal(false); // make sure it just a name and not js object
+          expect(name.length > 2).to.equal(true);
+          expect("test").to.equal("fuck");
+        })
     });
   });
   it('/all returns json', () => {
@@ -71,7 +85,7 @@ describe('Server Module', function () {
         .then(test => {
           server.close();
           return test;
-        });
+        }).catch(e => server.close());
     });
   });
 });
